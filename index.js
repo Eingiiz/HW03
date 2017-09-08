dd7782c43e16481972b6e667993863450dbdf422
Eingiiz <31319372+Eingiiz@users.noreply.github.com>
2017-09-08 23:51:04 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,131 +1,67 @@
-const beerAPI = `https://api.punkapi.com/v2/beers`
-const getAll = () => {
-    return fetch(`${beerAPI}`)
-        .then(response => response.json())
-        .catch(error => error)
-}
-const get = (id) => {
-    return fetch(`${beerAPI}/${id}`)
-        .then(response => response.json())
-        .catch(error => error)
-}
-const getByParam = (param) => {
-    return fetch(`${beerAPI}/?${param}`)
-        .then(response => response.json())
-        .catch(error => error)
-}
-
-const card = (beer) => {
-    return `
-<div class="card" style="min-width: 250px; height: fit-content; margin-bottom: 10px">
-    <img class="card-img-top" src="${beer.image_url}" style="max-width: 50px; padding-top: 20px; margin:auto;">
-    <div class="card-body">
-        <h4 class="card-title">${beer.name}</h4>
-        <p class="card-text">${beer.description}</p>
-        <a href="#" class="btn btn-primary float-right" data-toggle="modal" data-target="#showDetail" onclick="showDetail(${beer.id})">More Detail</a>
-    </div>
-</div>
-`
-}
-
-const showDetail = (id) => {
-    get(id).then(beers => {
-        const beer = beers[0]
-        document.getElementById(`beer-name`).innerText = beer.name
-        let foodList = ''
-        for (let food of beer.food_pairing) {
-            foodList += `<li>${food}</li>`
+const getBeersAPI = () => fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .catch(e => console.err('Fail'))
+    .then(response => {
+        let obj = response;
+        let txt = "<table style='width: 60%;margin: auto;' >"; 
+			txt += "<tr style='background-color: #FFE4B5;'>";
+			txt += "<th>" + "Image" + "</th>";
+			txt += "<th>" + "Beer name" + "</th>";
+			txt += "<th>" + "Description" + "</th>";
+			txt += "<th>" + "Yeast" + "</th>";
+			txt += "</tr>";
+        for (let i = 0; i < obj.length; i++) {
+		txt += "<tr>";
+		txt += "<td>" + "<image src =" + obj[i].image_url + " style='width:20%;height:auto;'/></td>";
+        txt += "<td>" + obj[i].name + "</td>";
+        txt += "<td>" + obj[i].description + "</td>";
+		txt += "<td>" + obj[i].ingredients.yeast + "</td>";
+        txt += "</tr>";
+     
         }
-        document.getElementById(`beer-content`).innerHTML = `
-        <div class="text-center">
-            <img src="${beer.image_url}" style="max-height: 150px;">
-        </div>
-        <p style="text-indent: 50px;">${beer.description}</p>
-        <p><b>Tagline</b>: ${beer.tagline}</p>
-        <p><b>First Brewed</b>: ${beer.first_brewed}</p>
-        <p><b>Yeast</b>: ${beer.ingredients.yeast}</p>
-        <p><b>Food Pairing</b>:</p>
-        <ul>${foodList}</ul>
-        <p><b>Brewers Tips</b>: ${beer.brewers_tips}</p>
-        `
+		txt += "</table>";
+        document.getElementById("table_result").innerHTML = txt;
     })
-}
 
-const showResult = (beers) => {
-    let cards = '';
-    for (let beer of beers) {
-        cards += card(beer)
-    }
-    document.getElementById(`show-result`).innerHTML = cards ? cards : 'No result'
-}
-
-const search = () => {
-    let inputText = ''
-    let selectedBrewed = ''
-    let inputBrewedDate = ''
-    inputText = document.getElementById(`inputText`).value;
-    selectedBrewed = document.getElementById(`brewed`).value
-    inputBrewedDate = document.getElementById(`input-brewed-date`).value
-    if (!inputText) {
-        if (inputBrewedDate) {
-            const param = /[0-9]{2}-[0-9]{4}/g.exec(inputBrewedDate)
-            if (!param) {
-                showResult([])
-                return
-            }
-            if (selectedBrewed === 'be') {
-                getByParam(`brewed_before=${param[0]}`).then(beers => showResult(beers))
-            } else {
-                getByParam(`brewed_after=${param[0]}`).then(beers => showResult(beers))
-            }
-        } else {
-            showResult([])
-            return
+const getBeerAPIByName = (name) => fetch(`https://api.punkapi.com/v2/beers/?beer_name=${name}`)
+    .then(response => response.json())
+    .catch(e => console.err('Fail'))
+    .then(response => {
+        let obj = response;
+        let txt = "<table style='width: 60%;margin: auto;' >"; 
+			txt += "<tr style='background-color: #FFE4B5;'>";
+			txt += "<th>" + "Image" + "</th>";
+			txt += "<th>" + "Beer name" + "</th>";
+			txt += "<th>" + "Description" + "</th>";
+			txt += "<th>" + "Yeast" + "</th>";
+			txt += "</tr>";
+        for (let i = 0; i < obj.length; i++) {
+		txt += "<tr>";
+		txt += "<td>" + "<image src =" + obj[i].image_url + " style='width:20%;height:auto;'/></td>";
+        txt += "<td>" + obj[i].name + "</td>";
+        txt += "<td>" + obj[i].description + "</td>";
+		txt += "<td>" + obj[i].ingredients.yeast + "</td>";
+        txt += "</tr>";
+     
         }
+		txt += "</table>";
+        document.getElementById("table_result").innerHTML = txt;
+    })
+
+getBeersAPI()
+
+const toSearch = (name) => {
+    if (name != "") {
+       getBeerAPIByName(name)
     } else {
-        let param = ''
-        if (inputBrewedDate) {
-            const paramDate = /[0-9]{2}-[0-9]{4}/g.exec(inputBrewedDate)
-            if (paramDate && paramDate == inputBrewedDate) {
-                const select = selectedBrewed === 'be' ? 'brewed_before=' : 'brewed_after='
-                param = inputText.trim().split(' ').join('_').concat(`&${select}=${paramDate[0]}`)
-            } else {
-                showResult([])
-                return
-            }
-        } else {
-            param = inputText.trim().split(' ').join('_')
-        }
-        let beers = [];
-        getByParam(`beer_name=${param}`)
-            .then(b => beers.push(b))
-            .then(() => {
-                getByParam(`yeast=${param}`).then(b => beers.push(b))
-                    .then(() => {
-                        getByParam(`food=${param}`).then(b => beers.push(b))
-                            .then(() => {
-                                let result = []
-                                for (let arr of beers) {
-                                    for (let b of arr) {
-                                        let unique = true;
-                                        for (let i = 0; i < result.length; i++) {
-                                            if (result[i].id == b.id) {
-                                                unique = false
-                                                break
-                                            }
-                                        }
-                                        if (unique) {
-                                            result.push(b)
-                                        }
-                                    }
-                                }
-                                showResult(result)
-                            })
-                    })
-            })
+        let txt = "<table style='width: 60%;margin: auto;' >"; 
+			txt += "<tr style='background-color: #FFE4B5;'>";
+			txt += "<th>" + "Image" + "</th>";
+			txt += "<th>" + "Beer name" + "</th>";
+			txt += "<th>" + "Description" + "</th>";
+			txt += "<th>" + "Yeast" + "</th>";
+			txt += "</tr>";
+		txt += "</table>";
+        document.getElementById("table_result").innerHTML = txt;
     }
 }
-
-getAll().then(beers => {
-    showResult(beers)
-})
